44252bdb7bf274ea8c365085f5b8043ca2231553
batz <batman900@gmail.com>
2024-01-04 19:48:45 -0800

    needs optimization

--- binpack.go
+++ binpack.go
@@ -1,47 +1,454 @@
 package main;
 
 import (
-	"atp_planner/binpack2d"
+	//"atp_planner/binpack2d"
+	"atp_planner/fixedset"
+	"bufio"
+	"flag"
 	"fmt"
+	"math"
+	"os"
+	//"sort"
+	"strconv"
+	"strings"
+	"time"
 )
 
-type Area struct {
-	width, height int
+var input = flag.String("input", "", "the file to read workouts from")
+var typ = flag.String("type", "bike", "which type of workout to plan for")
+var phases = flag.String("phase", "Base", "the phase of the ATP")
+var hoursByWeek = flag.String("hours", "14h", "the goal hours for the week")
+var TSSByWeek = flag.String("tss", "870", "the goal tss for the week")
+var maxWorkouts = flag.Int("max_workouts", 4, "how many workouts to find")
+
+// TODO:
+// Take a file with phase, hours
+const maxNonLongWorkout = 1 * time.Hour + 45 * time.Minute;
+
+// Breakdown of percentages (50 == 50%) of time for each sport
+var WeeklyBreakdown = map[string]map[string]int{
+	"Base": {
+		"bike": 50,
+		"run": 30,
+		"swim": 20,
+	},
+	"Build": {
+		"bike": 52,
+		"run": 31,
+		"swim": 17,
+	},
+	"Peak": {
+		"bike": 52,
+		"run": 31,
+		"swim": 17,
+	},
+	"Race": {
+		"bike": 52,
+		"run": 31,
+		"swim": 17,
+	},
+	"Transition": {
+		"bike": 50,
+		"run": 30,
+		"swim": 20,
+	},
+};
+
+// Distribution represents the percentage of time spent in each zoen
+type Distribution struct {
+	Zone1 int
+	Zone2 int
+	Zone3 int
+	Zone4 int
+	Zone5a int
+	Zone5b int
+	Zone5c int
+};
+
+// The distribution expected for each phase
+var distribution = map[string]Distribution{
+	"Base": Distribution{
+		Zone1: 80,
+		Zone2: 0,
+		Zone3: 10,
+		Zone4: 7,
+		Zone5a: 2,
+		Zone5b: 1,
+		Zone5c: 0,
+	},
+	"Build": Distribution{
+		Zone1: 66,
+		Zone2: 0,
+		Zone3: 23,
+		Zone4: 8,
+		Zone5a: 2,
+		Zone5b: 1,
+		Zone5c: 0,
+	},
+	"Peak": Distribution{
+		Zone1: 66,
+		Zone2: 0,
+		Zone3: 20,
+		Zone4: 10,
+		Zone5a: 3,
+		Zone5b: 1,
+		Zone5c: 0,
+	},
+	"Taper": Distribution{
+		Zone1: 90,
+		Zone2: 0,
+		Zone3: 0,
+		Zone4: 7,
+		Zone5a: 3,
+		Zone5b: 0,
+		Zone5c: 0,
+	},
+	"Race": Distribution{
+		Zone1: 90,
+		Zone2: 0,
+		Zone3: 0,
+		Zone4: 7,
+		Zone5a: 3,
+		Zone5b: 0,
+		Zone5c: 0,
+	},
+};
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Zones represents the time in the zones
+type Zones struct {
+	Z1, Z2, Z3, Z4, Z5a, Z5b, Z5c time.Duration
 }
 
-func New(w, h int) Area { return Area{w, h}; }
-
-// Basic idea assume 5 zones:
-// i := Z5
-// goal = []int{Z_1, ..., Z_5}
-// for each workout that satisfies Z_i:
-//    goal -= workout
-//    if goal == 0 || at max_sport: break
-//    recurse(goal, i-1)
-//    add to array
-// repeat for other sports, then binpack on the
-// cartesian product of the three
-
-func main() {
-	packer := binpack2d.Create(12, 12)
-	workouts := []Area{
-		New(5, 5),
-		New(6, 5),
-		New(7, 5),
-		New(8, 5),
-		New(9, 5),
-		New(10, 5),
+func (z *Zones) Add(o *Zones) Zones {
+	return Zones{
+		Z1: z.Z1 + o.Z1,
+		Z2: z.Z2 + o.Z2,
+		Z3: z.Z3 + o.Z3,
+		Z4: z.Z4 + o.Z4,
+		Z5a: z.Z5a + o.Z5a,
+		Z5b: z.Z5b + o.Z5b,
+		Z5c: z.Z5c + o.Z5c,
 	};
-	// TODO: Sort by area (or height), then try to fit in order
-	for _, w := range workouts {
-		rect, ok := packer.Insert(w.width, w.height, binpack2d.RULE_BEST_AREA_FIT);
-		fmt.Printf("Added: %v %v\n", rect, ok);
+}
+
+func (z *Zones) Sub(o *Zones) Zones {
+	return Zones{
+		Z1: z.Z1 - o.Z1,
+		Z2: z.Z2 - o.Z2,
+		Z3: z.Z3 - o.Z3,
+		Z4: z.Z4 - o.Z4,
+		Z5a: z.Z5a - o.Z5a,
+		Z5b: z.Z5b - o.Z5b,
+		Z5c: z.Z5c - o.Z5c,
+	};
+}
+
+func (z *Zones) Iterate() []time.Duration {
+	return []time.Duration{z.Z1, z.Z2, z.Z3, z.Z4, z.Z5a, z.Z5b, z.Z5c};
+}
+
+func (z *Zones) Less(o *Zones) bool {
+	if z.Z5c < o.Z5c { return true; }
+	if z.Z5b < o.Z5b { return true; }
+	if z.Z5a < o.Z5a { return true; }
+	if z.Z4 < o.Z4 { return true; }
+	if z.Z3 < o.Z3 { return true; }
+	if z.Z2 < o.Z2 { return true; }
+	if z.Z1 < o.Z1 { return true; }
+	return false;
+}
+
+func (z *Zones) String() string {
+	return strings.Join([]string{z.Z1.String(), z.Z2.String(), z.Z3.String(), z.Z4.String(), z.Z5a.String(), z.Z5b.String(), z.Z5c.String()}, "\t");
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Workout represents a single workout
+type Workout struct {
+	Type string
+	Name string
+	Details string
+	TotalDur time.Duration
+	NP float64
+	IF float64
+	TSS float64
+	Zones *Zones
+};
+
+func (w Workout) String() string {
+	return fmt.Sprintf("%s\t%s\t%s", w.Name, w.TotalDur, w.Zones)
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Workouts represents a combination of workouts
+type Workouts struct {
+	w []Workout;
+	eff float64;
+	tss float64;
+}
+func NewEmptyPlan() *Workouts {
+	return &Workouts{
+		w: make([]Workout, 0),
+		eff: 0.0,
+	}
+}
+func NewPlan(w []Workout, eff float64) *Workouts {
+	tss := 0.0
+	for _, t := range w {
+		tss += t.TSS
+	}
+	return &Workouts{
+		w: w,
+		eff: eff,
+		tss: tss,
+	}
+}
+func (w *Workouts) Len() int { return len(w.w); }
+func (w *Workouts) Efficiency() float64 { return w.eff; }
+func (w *Workouts) String() string {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("Eff: %0.2f, tss: %0.2f\n", w.eff, w.tss))
+	for _, o := range w.w {
+		sb.WriteString(o.String())
+		sb.WriteString("\n")
+	}
+	return sb.String()
+}
+
+// Unused, will be used when optimized
+//type WeeklyPlan []*Workouts
+//func (w WeeklyPlan) Len() int      { return len(w) }
+//func (w WeeklyPlan) Swap(i, j int) { w[i], w[j] = w[j], w[i] }
+//func (w WeeklyPlan) Less(i, j int) bool {
+	//if math.Abs(w[i].eff - w[j].eff) < 1e-9 {
+		//return w[i].tss > w[j].tss
+	//}
+	//return w[i].eff < w[j].eff
+//}
+
+///////////////////////////////////////////////////////////////////////////////
+// WorkoutMap maps a type to a set of workouts
+type WorkoutMap map[string]*Workouts;
+
+///////////////////////////////////////////////////////////////////////////////
+// loadWorkouts reads the set of workouts from a file (TSV).
+// <type, name, description, duration, np, if, tss, zones (in seconds)>
+func loadWorkouts(input string) (WorkoutMap, error) {
+	ret := WorkoutMap{};
+	readFile, err := os.Open(input)
+	if err != nil {
+		return WorkoutMap{}, fmt.Errorf("Could not read file [%s]: %v\n", input, err);
+	}
+	scanner := bufio.NewScanner(readFile)
+	scanner.Scan();  // First line is header
+	for scanner.Scan() {
+		line := scanner.Text();
+		data := strings.Split(line, "\t");
+		if len(data) != 14 {
+			return WorkoutMap{}, fmt.Errorf("Could not parse line: [%s]\n", line);
+		}
+		dur, err := strconv.Atoi(data[3]);
+		if err != nil {
+			return WorkoutMap{}, fmt.Errorf("Failed to parse duration: %s due to %w", line, err);
+		}
+		np, err := strconv.ParseFloat(data[4], 64);
+		if err != nil {
+			return WorkoutMap{}, fmt.Errorf("Failed to parse np: %s due to %w", line, err);
+		}
+		iF, err := strconv.ParseFloat(data[5], 64);
+		if err != nil {
+			return WorkoutMap{}, fmt.Errorf("Failed to parse if: %s due to %w", line, err);
+		}
+		tss, err := strconv.ParseFloat(data[6], 64);
+		if err != nil {
+			return WorkoutMap{}, fmt.Errorf("Failed to parse tss: %s due to %w", line, err);
+		}
+		zones := make([]time.Duration, 7);
+		for i := 0; i < len(zones); i++ {
+			dur, err := strconv.Atoi(data[7+i]);
+			if err != nil {
+				return WorkoutMap{}, fmt.Errorf("Failed to parse Z%d: %s due to %w", (i+1), line, err);
+			}
+			zones[i] = time.Duration(dur) * time.Second;
+		}
+		w, ok := ret[data[0]];
 		if !ok {
-			break
+			w = NewEmptyPlan();
+			ret[data[0]] = w;
+		}
+		w.w = append(w.w, Workout{
+			Type: data[0],
+			Name: data[1],
+			Details: data[2],
+			TotalDur: time.Duration(dur) * time.Second,
+			NP: np,
+			IF: iF,
+			TSS: tss,
+			Zones: &Zones{
+				Z1: zones[0],
+				Z2: zones[1],
+				Z3: zones[2],
+				Z4: zones[3],
+				Z5a: zones[4],
+				Z5b: zones[5],
+				Z5c: zones[6],
+			},
+		});
+		ret[data[0]] = w;
+	}
+	return ret, nil
+}
+
+func multDur(d time.Duration, frac int) time.Duration {
+	return time.Duration(float64(d.Seconds()) * float64(frac) / 100.0) * time.Second;
+}
+
+func diff(observed, expected float64) float64 {
+	f := math.Max(expected, 60.0)
+	return math.Abs(observed - expected) / math.Abs(f)
+}
+
+func efficiency(observed, desired Zones) float64 {
+	return math.Pow((observed.Z1.Seconds() + observed.Z2.Seconds()) - (desired.Z1.Seconds() + desired.Z2.Seconds()), 2) +
+		math.Pow(observed.Z3.Seconds() - desired.Z3.Seconds(), 2) +
+		math.Pow(observed.Z4.Seconds() - desired.Z4.Seconds(), 2) +
+		math.Pow(observed.Z5a.Seconds() - desired.Z5a.Seconds(), 2) +
+		math.Pow(observed.Z5b.Seconds() - desired.Z5b.Seconds(), 2) +
+		math.Pow(observed.Z5c.Seconds() - desired.Z5c.Seconds(), 2)
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// enumerate finds the best workouts matching the given goal of Zones
+var best *fixedset.FixedSet[*Workouts]
+func enumerate(foundLong bool, maxWorkouts int, goal Zones, all, list *Workouts) {
+	// TODO:
+	// optimize
+	// make recursive
+	totalCount := 0
+	maxIterations := math.Pow(float64(len(all.w)), float64(maxWorkouts))
+	toPrint := int(maxIterations / 100.0)
+	nextPrint := toPrint
+	fmt.Fprintf(os.Stderr, "maxIterations: %0.2f, toPrint: %d, nextPrint: %d\n", maxIterations, toPrint, nextPrint)
+	for i := 0; i < len(all.w); i++ {
+		a := all.w[i].Zones
+		foundLong := all.w[i].TotalDur > maxNonLongWorkout
+		for j := i+1; j < len(all.w); j++ {
+			if all.w[j].TotalDur > maxNonLongWorkout && foundLong {
+				totalCount++
+				continue
+			}
+			foundLong = foundLong || all.w[j].TotalDur > maxNonLongWorkout
+			b := a.Add(all.w[j].Zones)
+			for k := j+1; k < len(all.w); k++ {
+				if all.w[k].TotalDur > maxNonLongWorkout && foundLong {
+					totalCount++
+					continue
+				}
+				foundLong = foundLong || all.w[k].TotalDur > maxNonLongWorkout
+				c := b.Add(all.w[k].Zones)
+				for l := k+1; l < len(all.w); l++ {
+					if all.w[l].TotalDur > maxNonLongWorkout && foundLong {
+						totalCount++
+						continue
+					}
+					d := c.Add(all.w[l].Zones)
+					list := NewPlan([]Workout{all.w[i], all.w[j], all.w[k], all.w[l]}, efficiency(d, goal))
+					best.Add(list);
+					totalCount++
+					if totalCount > nextPrint {
+						nextPrint += toPrint
+						fmt.Fprintf(os.Stderr, "nextPrint: %d\n", nextPrint)
+						fmt.Fprintf(os.Stderr, "Done: %0.2f\n", 100.0 * float64(totalCount) / float64(maxIterations));
+					}
+				}
+			}
 		}
 	}
-	packer.ShrinkBin(false)
-	for i := 0; i < packer.GetUsedRectanglesLength(); i += 1 {
-		fmt.Printf("Rect[%d]=%v\n", i, packer.GetUsedRectangle(i));
+}
+
+func main() {
+	flag.Parse()
+	workoutMap, err := loadWorkouts(*input);
+	if err != nil {
+		fmt.Printf("%v", err);
+		return;
+	}
+	workouts, ok := workoutMap[*typ];
+	if !ok {
+		fmt.Printf("Unknown type: %v\n", *typ);
+		return;
+	}
+	fmt.Printf("Have: %d workouts (enumerating %0.2f times)\n", workouts.Len(), math.Pow(float64(workouts.Len()), float64(*maxWorkouts)))
+	p := strings.Split(*phases, ",")
+	h := strings.Split(*hoursByWeek, ",")
+	t := strings.Split(*TSSByWeek, ",")
+	if len(p) != len(h) || len(h) != len(p) {
+		fmt.Fprintf(os.Stderr, "Arrays must be the same length: phase=%d hours=%d tss=%d\n", len(p), len(h), len(t))
+		os.Exit(1)
+	}
+	for i := 0; i < len(p); i++ {
+		phase := p[i]
+		hours, err := time.ParseDuration(h[i])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not parse hours: %s\n", err)
+		}
+		tss, err := strconv.ParseFloat(t[i], 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not parse tss: %s\n", err)
+		}
+		dist, ok := distribution[phase];
+		if !ok {
+			fmt.Printf("Unknown phase: %v\n", phase);
+			return;
+		}
+		weekDist, ok := WeeklyBreakdown[phase];
+		if !ok {
+			fmt.Printf("Unknown phase (weekly): %v\n", phase);
+			return;
+		}
+		frac := float64(weekDist[*typ]) / 100.0;
+		goal := Zones{
+			Z1: multDur(time.Duration(float64(hours)*frac), dist.Zone1),
+			Z2: multDur(time.Duration(float64(hours)*frac), dist.Zone2),
+			Z3: multDur(time.Duration(float64(hours)*frac), dist.Zone3),
+			Z4: multDur(time.Duration(float64(hours)*frac), dist.Zone4),
+			Z5a: multDur(time.Duration(float64(hours)*frac), dist.Zone5a),
+			Z5b: multDur(time.Duration(float64(hours)*frac), dist.Zone5b),
+			Z5c: multDur(time.Duration(float64(hours)*frac), dist.Zone5c),
+		}
+		goalTSS := tss * frac
+		best = fixedset.New[*Workouts](10, func(a, b *Workouts) bool {
+			return a.Efficiency() < b.Efficiency()
+		})
+		enumerate(false, *maxWorkouts, goal, workouts, NewEmptyPlan())
+		// Find the closest to the goal TSS
+		var bestWorkouts []Workout
+		bestDiff := math.MaxFloat64
+		for _, b := range best.Iterate() {
+			tss := 0.0;
+			for _, w := range b.w {
+				tss += w.TSS;
+			}
+			d := diff(tss, goalTSS)
+			if d < bestDiff {
+				bestWorkouts = b.w
+				bestDiff = d
+			}
+		}
+		fmt.Printf("Week[%d]:\n", i)
+		bestTSS := 0.0;
+		total := time.Duration(0)
+		for _, w := range bestWorkouts {
+			star := ""
+			if w.TotalDur > maxNonLongWorkout {
+				star = "*"
+			}
+			fmt.Printf("\t%s%s\t\t%s\t%s\n", star, w.Name, w.TotalDur, w.Zones.String());
+			bestTSS += w.TSS;
+			total += w.TotalDur
+		}
+		fmt.Printf("total: %s (goal: %s)\ntss: %0.2f (goal: %0.2f)\ndiff: %0.2f\n\n", total, time.Duration(float64(hours)*frac), bestTSS, tss, bestDiff)
 	}
 }
--- tss_calculator.go
+++ tss_calculator.go
@@ -1,14 +1,15 @@
-package main;
+package main
 
 import (
+	"atp_planner/parser"
 	"bufio"
 	"flag"
 	"fmt"
+	"github.com/antlr4-go/antlr/v4"
 	"math"
 	"os"
-	"strings"
 	"sort"
-	"atp_planner/parser"
+	"strings"
 )
 
 var input = flag.String("input", "", "the file to read workouts from")
@@ -22,9 +23,9 @@ var zones = parser.ZoneMap{
 	"Z5a": parser.Zone{104, 119},  // VO2 Max
 	"Z5b": parser.Zone{119, 150},  // Anarobic
 	"Z5c": parser.Zone{150, 200},  // Neuromuscular
-};
+}
 
-const thresholdPace = 10; // mph
+const thresholdPace = 6 // min / mile
 var zonesRun = parser.ZoneMap{
 	"Z1": parser.Zone{60, 74},     // Active Recovery
 	"Z2": parser.Zone{73, 82},     // Endurance
@@ -33,98 +34,133 @@ var zonesRun = parser.ZoneMap{
 	"Z5a": parser.Zone{100, 103},  // VO2 Max
 	"Z5b": parser.Zone{103, 110},  // Anarobic
 	"Z5c": parser.Zone{111, 125},  // Neuromuscular
-};
+}
 
-func analyzeWorkout(typ, name, input string, mult float64, zones parser.ZoneMap, sortedZoneNames []string) (string, error) {
-	toks, err := parser.Tokenize(input);
-	if err != nil {
-		return "", fmt.Errorf("Failed to tokenize: %v", err);
+///////////////////////////////////////////////////////////////////////////////
+type ErrorListener struct {
+	*antlr.DiagnosticErrorListener
+}
+func NewErrorListener() *ErrorListener {
+	return &ErrorListener{antlr.NewDiagnosticErrorListener(true)}
+}
+func (l *ErrorListener) SyntaxError(recognizer antlr.Recognizer, offendingSymbol interface{}, line, column int, msg string, e antlr.RecognitionException) {
+	fmt.Printf("SyntaxError\n")
+	os.Exit(1)
+}
+func (l *ErrorListener) ReportAmbiguity(recognizer antlr.Parser, dfa *antlr.DFA, startIndex, stopIndex int, exact bool, ambigAlts *antlr.BitSet, configs *antlr.ATNConfigSet) {
+	fmt.Printf("ReportAmbiguity\n")
+	os.Exit(1)
+}
+func (l *ErrorListener) ReportAttemptingFullContext(recognizer antlr.Parser, dfa *antlr.DFA, startIndex, stopIndex int, conflictingAlts *antlr.BitSet, configs *antlr.ATNConfigSet) {
+	fmt.Printf("ReportAttemptingFullContext\n")
+	os.Exit(1)
+}
+func (l *ErrorListener) ReportContextSensitivity(recognizer antlr.Parser, dfa *antlr.DFA, startIndex, stopIndex, prediction int, configs *antlr.ATNConfigSet) {
+	fmt.Printf("ReportContextSensitivity\n")
+	os.Exit(1)
+}
+
+///////////////////////////////////////////////////////////////////////////////
+func parseInput(name, input string) parser.Interval {
+	fmt.Fprintf(os.Stderr, "Parsing: %s\n", name);
+	is := antlr.NewInputStream(input)
+	lexer := parser.NewCalcLexer(is)
+	lexer.AddErrorListener(NewErrorListener())
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+	p := parser.NewCalcParser(stream)
+	p.SetErrorHandler(antlr.NewBailErrorStrategy())
+	p.Start_()
+	if p.HasError() {
+		fmt.Fprintf(os.Stderr, "Parse failed: %v\n", p.GetError())
+		os.Exit(1)
 	}
-	p := parser.New(toks, zones);
-	i, err := p.Parse();
-	if err != nil {
-		return "", fmt.Errorf("Failed to parse: %v", err);
-	}
+	return parser.Result
+}
+
+func analyzeWorkout(typ parser.IntervalType, name, input string, p parser.PowerData, sortedZoneNames []string) (string, error) {
 	// Compute statistics
-	w := i.Window1s();
-	w30 := []float64{}
-	for i := 0; i < len(w); i += 30 {
-		sum := 0.0;
-		for j := 0; j < 30; j += 1 {
-			sum += mult * float64(w[i+j].PowerLow + w[i+j].PowerHigh) / 200.0;
-		}
-		w30 = append(w30, sum/30.0)
-	}
+	w := parseInput(name, input).Window1s(typ, p)
 	cnt := 0
-	sum := 0.0;
-	for i := 2; i < len(w30); i += 1 {
+	sum := 0.0
+	for i := 0; i < len(w)-30; i++ {
 		tsum := 0.0
-		for j := 2; j >= 0; j -= 1 {
-			tsum += w30[i-j];
+		for j := i; j < i+30 && j < len(w); j++ {
+			tsum += w[j]
 		}
-		sum += math.Pow(tsum / 3.0, 4.0);
-		cnt += 1;
+		sum += math.Pow(tsum / 30.0, 4.0)
+		cnt++
 	}
-	np := math.Round(math.Pow(sum / float64(cnt), 1.0/4.0)*100) / 100;
-	intensity := np / mult;
+	mult := parser.Multiplier(typ, p)
+	np := math.Round(math.Pow(sum / float64(cnt), 1.0/4.0)*100) / 100
+	intensity := np / mult
 	// (# of seconds of the workout x Normalized Power x Intensity Factor) / (FTP x 3600) x 100
-	tss := (float64(len(w)) * np * intensity / (mult * 3600)) * 100;
+	tss := (float64(len(w)) * np * intensity / (mult * 3600)) * 100
 	// Print results
-	body := []string{typ, name, input, fmt.Sprintf("%d", len(w))};
-	body = append(body, fmt.Sprintf("%0.2f", np), fmt.Sprintf("%0.2f", intensity), fmt.Sprintf("%0.2f", tss));
+	body := []string{typ.String(), name, input, fmt.Sprintf("%d", len(w))}
+	body = append(body, fmt.Sprintf("%0.2f", np), fmt.Sprintf("%0.2f", intensity), fmt.Sprintf("%0.2f", tss))
 	timeInZones := map[string]int64{}
 	for k := range zones {
-		timeInZones[k] = 0;
+		timeInZones[k] = 0
 	}
 	for _, v := range w {
-		timeInZones[p.ZoneToName(float64(v.PowerLow + v.PowerHigh) / 2.0)] += 1;
+		timeInZones[p.ZoneToName(typ, v)] += 1
 	}
 	for _, v := range sortedZoneNames {
-		body = append(body, fmt.Sprintf("%d", timeInZones[v]));
+		body = append(body, fmt.Sprintf("%d", timeInZones[v]))
 	}
-	return strings.Join(body, "\t"), nil;
+	return strings.Join(body, "\t"), nil
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 func main() {
 	flag.Parse()
-	zoneNames := []string{};
+	zoneNames := []string{}
 	for k := range zones {
-		zoneNames = append(zoneNames, k);
+		zoneNames = append(zoneNames, k)
 	}
-	sort.Strings(zoneNames);
+	sort.Strings(zoneNames)
 	hdr := []string{"Type", "Name", "Details", "Total (s)", "NP", "IF", "TSS"}
-	hdr = append(hdr, zoneNames...);
-	fmt.Printf("%s\n", strings.Join(hdr, "\t"));
+	hdr = append(hdr, zoneNames...)
+	fmt.Printf("%s\n", strings.Join(hdr, "\t"))
 	// Read file
 	readFile, err := os.Open(*input)
 	if err != nil {
-		fmt.Printf("Could not read file [%s]: %v\n", *input, err);
-		return;
+		fmt.Printf("Could not read file [%s]: %v\n", *input, err)
+		return
 	}
 	scanner := bufio.NewScanner(readFile)
 	for scanner.Scan() {
-		line := scanner.Text();
-		data := strings.Split(line, "\t");
+		line := scanner.Text()
+		data := strings.Split(line, "\t")
 		if len(data) != 3 {
-			fmt.Printf("Could not parse line: [%s]\n", line);
-			return;
+			fmt.Printf("Could not parse line: [%s]\n", line)
+			return
 		}
-		mult := ftp
-		tz := zones;
-		if data[0] == "run" {
-			mult = thresholdPace;
-			tz = zonesRun;
+		p := parser.PowerData {
+			BikeFTP: ftp,
+			BikeZones: zones,
+			RunPace: thresholdPace,
+			RunZones: zonesRun,
 		}
-		body, err := analyzeWorkout(data[0], data[1], data[2], mult, tz, zoneNames);
+		var typ parser.IntervalType
+		switch data[0] {
+		case parser.BikeInterval.String():
+			typ = parser.BikeInterval
+		case parser.RunInterval.String():
+			typ = parser.RunInterval
+		default:
+			fmt.Fprintf(os.Stderr, "unknown type: %v in [%s]\n", typ, line)
+			os.Exit(1)
+		}
+		body, err := analyzeWorkout(typ, data[1], data[2], p, zoneNames)
 		if err != nil {
-			fmt.Printf("%v\n", err);
-			return;
+			fmt.Printf("%v\n", err)
+			return
 		}
-		fmt.Printf("%s\n", body);
+		fmt.Printf("%s\n", body)
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		os.Exit(1)
 	}
-	// TODO test a distance workout
 }
